a5350de934a17a6df496d946a50bc2f3aa9281a2
docdemon09 <www.sayannandi12@gmail.com>
2025-01-16 22:11:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.querySelector('.homepage').style.display = 'none';
+    document.getElementById('surprisePage').style.display = 'block';
+});
